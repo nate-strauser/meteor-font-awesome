3b1e1ba4cab542ce7b9a47ff61355d4b795d0951
Nathan Strauser <nathan.strauser@gmail.com>
2013-01-18 11:39:20 -0500

    package tweak

--- package.js
+++ package.js
@@ -2,13 +2,11 @@ Package.describe({
 	summary: "Latest version Font-Awesome"
 });
 
-Package.on_use(function(api){
-	api.add_files([
-		'font-awesome/font/fontawesome-webfont.eot',
-		'font-awesome/font/FontAwesome.otf',
-		'font-awesome/font/fontawesome-webfont.ttf',
-		'font-awesome/font/fontawesome-webfont.woff',
-		'font-awesome/font/fontawesome-webfont.svg',
-		'font-awesome/css/font-awesome.css'
-	], 'client');
+Package.on_use(function (api){
+	api.add_files('lib/font-awesome/font/fontawesome-webfont.eot', 'client');
+	api.add_files('lib/font-awesome/font/FontAwesome.otf', 'client');
+	api.add_files('lib/font-awesome/font/fontawesome-webfont.ttf', 'client');
+	api.add_files('lib/font-awesome/font/fontawesome-webfont.woff', 'client');
+	api.add_files('lib/font-awesome/font/fontawesome-webfont.svg', 'client');
+	api.add_files('lib/font-awesome/css/font-awesome.css', 'client');
 });
