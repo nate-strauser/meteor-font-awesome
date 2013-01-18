b13d67d75320c2eb0f12070d7c00bbb7caa3a488
Nathan Strauser <nathan.strauser@gmail.com>
2013-01-18 11:10:29 -0500

    support files

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ meteor-font-awesome
 ===================
 
 Meteorite smart package for font awesome
+[Font-Awesome 3.0](http://fortawesome.github.com/Font-Awesome/)
+
+## How to install
+1. `npm install -g meteorite` (if not already installed)
+2. `mrt add font-awesome`
---
+++ package.js
@@ -0,0 +1,15 @@
+Package.describe({
+	summary: "Latest version Font-Awesome"
+});
+
+Package.on_use(function(api){
+	api.use('jquery', 'client');
+	api.add_files([
+		'font-awesome/font/fontawesome-webfont.eot',
+		'font-awesome/font/FontAwesome.otf',
+		'font-awesome/font/fontawesome-webfont.ttf',
+		'font-awesome/font/fontawesome-webfont.woff',
+		'font-awesome/font/fontawesome-webfont.svg',
+		'font-awesome/css/font-awesome.css'
+	], 'client');
+});
