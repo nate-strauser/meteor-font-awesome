c54cf856cd5f790acb20b5f924746d3f6bd1fdc8
Nathan Strauser <nathan.strauser@gmail.com>
2013-05-22 16:16:18 -0400

    fix

--- package.js
+++ package.js
@@ -4,10 +4,10 @@ Package.describe({
 
 Package.on_use(function (api){
 	api.add_files('lib/font-awesome/font/fontawesome-webfont.eot', 'client');
-	api.add_files('lib/font-awesome/font/FontAwesome.otf', 'client');
+	api.add_files('lib/font-awesome/font/fontawesome-webfont.svg', 'client');
 	api.add_files('lib/font-awesome/font/fontawesome-webfont.ttf', 'client');
 	api.add_files('lib/font-awesome/font/fontawesome-webfont.woff', 'client');
-	api.add_files('lib/font-awesome/font/fontawesome-webfont.svg', 'client');
+	api.add_files('lib/font-awesome/font/FontAwesome.otf', 'client');
 	api.add_files('lib/font-awesome/css/font-awesome.css', 'client');
 	
 	//font path override
