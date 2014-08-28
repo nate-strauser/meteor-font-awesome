0fb4c58c2c9ab4e561341a7b5c7f2ddf171f9e86
Nathan Strauser <nathan.strauser@gmail.com>
2014-08-27 20:32:57 -0400

    0.9.0 update

--- package.js
+++ package.js
@@ -1,7 +1,11 @@
 Package.describe({
-	summary: "Latest version Font-Awesome loaded via CDN"
+	name: "natestrauser:font-awesome",
+  summary: "Latest version Font-Awesome loaded via CDN",
+  version: "4.1.1",
+  git: "https://github.com/nate-strauser/meteor-font-awesome.git"
 });
 
 Package.on_use(function (api){
+  api.versionsFrom("METEOR@0.9.0");
 	api.add_files('load.js', 'client');
 });
