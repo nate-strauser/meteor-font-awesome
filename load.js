541dee4368982f13f7b27b9fdc3c67b982e16fad
Nathan Strauser <nathan.strauser@gmail.com>
2016-05-26 10:13:04 -0400

    4.6.3 update

--- load.js
+++ load.js
@@ -4,6 +4,6 @@ var head = document.getElementsByTagName('head')[0];
 var style = document.createElement('link');
 style.type = 'text/css';
 style.rel = "stylesheet";
-style.href = '//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css';
+style.href = '//maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css';
 
 head.appendChild(style);
